5c53e546e7382e2ae93508b9c6f69bb7e85fe923
Mussa Charles <mussacharles60@gmail.com>
2021-10-19 22:37:40 +0300

    updated main component

--- src/components/main/index.tsx
+++ src/components/main/index.tsx
@@ -1,10 +1,23 @@
 import React from "react";
 import "./index.css";
 
-export default class Main extends React.Component<{}> {
-  // constructor(props: any) {
-  //     super(props);
-  // }
+export default class Main extends React.Component<
+  {},
+  {
+    user_id: string;
+  }
+> {
+  constructor(props: any) {
+    super(props);
+
+    this.state = {
+      user_id: '',
+    };
+
+    this.onUserIdChanged = this.onUserIdChanged.bind(this);
+  }
+
+  onUserIdChanged(event: any) {}
 
   render() {
     return (
@@ -13,6 +26,16 @@ export default class Main extends React.Component<{}> {
         <div className="main-lay">
           <div className="input-lay mdm-shadow">
             <div className="title-lay">Input User's Photos</div>
+            <div className="user-id-lay">
+              <input
+                className="user-id-input input"
+                name="email"
+                value={this.state.user_id}
+                onChange={this.onUserIdChanged}
+                type="text"
+                placeholder="Enter User Email Address"
+              />
+            </div>
           </div>
           <div className="trainer-lay mdm-shadow">
             <div className="title-lay">Train Model</div>
