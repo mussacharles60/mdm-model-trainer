1042c634b70cb36340a83c9708e2e823e4363404
Mussa Charles <mussacharles60@gmail.com>
2021-10-19 20:54:42 +0300

    updated main component

--- src/components/main/index.tsx
+++ src/components/main/index.tsx
@@ -1,28 +1,27 @@
-import React from 'react';
-import './index.css';
+import React from "react";
+import "./index.css";
 
 export default class Main extends React.Component<{}> {
+  // constructor(props: any) {
+  //     super(props);
+  // }
 
-    // constructor(props: any) {
-    //     super(props);
-    // }
-
-    render() {
-        return (
-            <div className="mdm-main">
-                <div className="header-main"></div>
-                <div className="main-lay">
-                    <div className="input-lay mdm-shadow">
-
-                    </div>
-                    <div className="trainer-lay mdm-shadow">
-
-                    </div>
-                    <div className="output-lay mdm-shadow">
-
-                    </div>
-                </div>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="mdm-main">
+        <div className="header-main"></div>
+        <div className="main-lay">
+          <div className="input-lay mdm-shadow">
+            <div className="title-lay">Input User's Photos</div>
+          </div>
+          <div className="trainer-lay mdm-shadow">
+            <div className="title-lay">Train Model</div>
+          </div>
+          <div className="output-lay mdm-shadow">
+            <div className="title-lay">Export User's Model</div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
