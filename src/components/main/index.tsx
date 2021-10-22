a194c3950326310fa461d71eefb76530be37f4de
Mussa Charles <mussacharles60@gmail.com>
2021-10-22 23:54:06 +0300

    update user interface

--- src/components/main/index.tsx
+++ src/components/main/index.tsx
@@ -17,7 +17,7 @@ export default class Main extends React.Component<
     session_id: string;
     input_image: string;
 
-    input_images: Array<string>;
+    is_initializing: boolean;
     is_running_process: boolean;
     error: string | null;
   }
@@ -26,6 +26,7 @@ export default class Main extends React.Component<
   // private recognizer: fr.FaceRecognizer = fr.FaceRecognizer();
   private mdmServer: MDMServer = new MDMServer();
   private sessionInterval: any = null;
+  private didMount: boolean = false;
 
   constructor(props: any) {
     super(props);
@@ -33,77 +34,26 @@ export default class Main extends React.Component<
     this.state = {
       session_id: "mdm_session_02",
       input_image: "",
-      input_images: [],
+      is_initializing: true,
       is_running_process: false,
       error: null,
     };
 
     this.onSessionIdChanged = this.onSessionIdChanged.bind(this);
-    this.onAddBtnClick = this.onAddBtnClick.bind(this);
-    this.handleFileChange = this.handleFileChange.bind(this);
-    this.onRemoveAllBtnClick = this.onRemoveAllBtnClick.bind(this);
-    this.onRemoveBtnClick = this.onRemoveBtnClick.bind(this);
     this.onStartSessionBtnClick = this.onStartSessionBtnClick.bind(this);
     this.onStopSessionBtnClick = this.onStopSessionBtnClick.bind(this);
   }
 
   async componentDidMount() {
+    this.didMount = true;
     await this.loadModels();
-    // console.log(faceapi.nets);
+    console.log(faceapi.nets);
+    this.setState({ is_initializing: false });
   }
 
   componentWillUnmount() {
-    if (this.sessionInterval) clearInterval(this.sessionInterval);
+    this.didMount = false;
   }
-
-  onSessionIdChanged(event: any) {
-    this.setState({ session_id: event.target.value });
-  }
-
-  onAddBtnClick() {
-    if (this.state.is_running_process) return;
-    const inputFile = document.getElementById("input-file");
-    if (inputFile) {
-      inputFile.click();
-    }
-  }
-
-  onRemoveAllBtnClick() {
-    if (this.state.is_running_process) return;
-    this.setState({ input_images: [] });
-  }
-
-  onRemoveBtnClick(index: number) {
-    if (this.state.is_running_process) return;
-    const images = this.state.input_images;
-    images.splice(index, 1);
-    this.setState({ input_images: images });
-  }
-
-  handleFileChange(files: FileList | null) {
-    console.log(files);
-    if (files && files.length > 0) {
-      const images: Array<string> = this.state.input_images;
-      for (var i = 0; i < files.length; i++) {
-        const file = files[i];
-        if (file) {
-          const filePath = URL.createObjectURL(file);
-          images.push(filePath);
-        }
-      }
-      this.setState({ input_images: images });
-      console.log(this.state);
-    }
-  }
-
-  private onStartSessionBtnClick() {
-    if (this.state.is_running_process) return;
-    if (this.state.session_id.length === 0) return;
-    // if (this.sessionInterval) clearInterval(this.sessionInterval);
-    this.setState({ is_running_process: true });
-    this.queSession(this.state.session_id);
-  }
-
   async loadModels() {
     return Promise.all([
       faceapi.nets.ssdMobilenetv1.loadFromUri(MODEL_URL),
@@ -114,10 +64,20 @@ export default class Main extends React.Component<
       faceapi.nets.ageGenderNet.loadFromUri(MODEL_URL),
     ]);
   }
+  onSessionIdChanged(event: any) {
+    this.setState({ session_id: event.target.value });
+  }
+
+  private onStartSessionBtnClick() {
+    if (this.state.is_running_process) return;
+    if (this.state.session_id.length === 0) return;
+    this.setState({ is_running_process: true });
+    this.queSession(this.state.session_id);
+  }
 
   private queSession(session_id: string) {
     this.mdmServer.getQueryPhoto(session_id).then((result) => {
-      // console.log("result: ", result);
+      console.log("result: ", result);
       if (
         result &&
         result.success &&
@@ -125,102 +85,116 @@ export default class Main extends React.Component<
         result.success.data.length > 0
       ) {
         const device: DeviceInfo | null = result.success.data[0];
-        if (device) {
-          const image_url =
-            photo_url + device.sessionId + "/" + device.fileName;
-
-          const imgElement = document.getElementById("input-img");
-          const canvasElement = document.getElementById("canvas");
-          const outCanvasElement = document.getElementById("canvas-output");
-          // this.drawHTMLImage(this.canvas.current, testImageHTML, 296, 296);
-          global.HTMLImageElement = window.HTMLImageElement;
-          // var image = new Image();
-          if (
-            imgElement &&
-            canvasElement &&
-            outCanvasElement &&
-            (imgElement as HTMLImageElement) &&
-            (canvasElement as HTMLCanvasElement) &&
-            (outCanvasElement as HTMLCanvasElement)
-          ) {
-            (imgElement as HTMLImageElement).onload = async () => {
-              this.mdmServer
-                .clearQuery(this.state.session_id)
-                .then((result) => {
-                  if (result && result.success) {
-                    console.log(result.success);
-                  } else if (result && result.error) {
-                    console.log(result.error);
-                  }
-                })
-                .catch((error) => {
-                  console.log(error);
-                });
-              //
-              (canvasElement as HTMLCanvasElement).width =
-                imgElement.getBoundingClientRect().width;
-              (canvasElement as HTMLCanvasElement).height =
-                imgElement.getBoundingClientRect().height;
-              const ctx = (canvasElement as HTMLCanvasElement).getContext("2d");
-              if (ctx)
-                ctx.drawImage(
-                  imgElement as HTMLImageElement,
-                  0,
-                  0,
-                  (canvasElement as HTMLCanvasElement).width,
-                  (canvasElement as HTMLCanvasElement).height
-                );
-              setTimeout(async () => {
-                const detectionResult = await faceapi
-                  .detectAllFaces(canvasElement as HTMLCanvasElement)
-                  .withFaceLandmarks()
-                  .withFaceExpressions()
-                  .withAgeAndGender()
-                  .withFaceDescriptors();
-
-                console.log("detectionResult: ", detectionResult);
-                if (detectionResult && detectionResult.length) {
-                  const displaySize = {
-                    width: (canvasElement as HTMLCanvasElement).width,
-                    height: (canvasElement as HTMLCanvasElement).height,
-                  };
-                  faceapi.matchDimensions((outCanvasElement as HTMLCanvasElement), displaySize);
-                  const resizedDetections = faceapi.resizeResults(detectionResult, displaySize);
-                  faceapi.draw.drawDetections((outCanvasElement as HTMLCanvasElement), resizedDetections);
-                  faceapi.draw.drawFaceLandmarks((outCanvasElement as HTMLCanvasElement), resizedDetections);
-                  const minProbability = 0.05
-                  faceapi.draw.drawFaceExpressions((outCanvasElement as HTMLCanvasElement), resizedDetections, minProbability)
-                  // const descriptor = singleResult[0];
-                  const faceMatcher = new faceapi.FaceMatcher(detectionResult);
-                  // const bestMatch = faceMatcher.findBestMatch(singleResult.descriptor);
-                  console.log(faceMatcher); //output
-                  setTimeout(() => {
-                    if (this.state.is_running_process)
-                      this.queSession(this.state.session_id);
-                  }, 500);
-                } else {
-                  setTimeout(() => {
-                    if (this.state.is_running_process)
-                      this.queSession(this.state.session_id);
-                  }, 500);
-                }
-              }, 1000);
-            };
-            this.setState({ input_image: image_url });
-          }
+        if (device && this.state.is_running_process) {
+          this.processResult(device);
         } else {
-          setTimeout(() => {
-            if (this.state.is_running_process)
-              this.queSession(this.state.session_id);
-          }, 500);
+          this.reQueSession();
         }
       } else {
-        setTimeout(() => {
-          if (this.state.is_running_process)
-            this.queSession(this.state.session_id);
-        }, 100);
+        this.reQueSession();
       }
     });
+  }
+
+  private processResult(device: DeviceInfo) {
+    const image_url = photo_url + device.sessionId + "/" + device.fileName;
+    const imgElement = document.getElementById("input-img");
+    const canvasElement = document.getElementById("canvas");
+    const outCanvasElement = document.getElementById("canvas-output");
+    // global.HTMLImageElement = window.HTMLImageElement;
+    if (
+      imgElement &&
+      canvasElement &&
+      outCanvasElement &&
+      (imgElement as HTMLImageElement) &&
+      (canvasElement as HTMLCanvasElement) &&
+      (outCanvasElement as HTMLCanvasElement)
+    ) {
+      (imgElement as HTMLImageElement).onload = async () => {
+        if (!this.didMount) return;
+        this.mdmServer
+          .clearQuery(this.state.session_id)
+          .then((result) => {
+            if (result && result.success) {
+              console.log(result.success);
+            } else if (result && result.error) {
+              console.log(result.error);
+            }
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+        //
+        (canvasElement as HTMLCanvasElement).width =
+          imgElement.getBoundingClientRect().width;
+        (canvasElement as HTMLCanvasElement).height =
+          imgElement.getBoundingClientRect().height;
+        const ctx = (canvasElement as HTMLCanvasElement).getContext("2d");
+        if (ctx)
+          ctx.drawImage(
+            imgElement as HTMLImageElement,
+            0,
+            0,
+            (canvasElement as HTMLCanvasElement).width,
+            (canvasElement as HTMLCanvasElement).height
+          );
+        const detectionResult = await faceapi
+          .detectAllFaces(canvasElement as HTMLCanvasElement)
+          .withFaceLandmarks()
+          .withFaceExpressions()
+          .withAgeAndGender()
+          .withFaceDescriptors();
+
+        console.log("detectionResult: ", detectionResult);
+        if (detectionResult && detectionResult.length) {
+          if (!this.didMount) return;
+          const displaySize = {
+            width: (canvasElement as HTMLCanvasElement).width,
+            height: (canvasElement as HTMLCanvasElement).height,
+          };
+          faceapi.matchDimensions(
+            outCanvasElement as HTMLCanvasElement,
+            displaySize
+          );
+          const resizedDetections = faceapi.resizeResults(
+            detectionResult,
+            displaySize
+          );
+          faceapi.draw.drawDetections(
+            outCanvasElement as HTMLCanvasElement,
+            resizedDetections
+          );
+          faceapi.draw.drawFaceLandmarks(
+            outCanvasElement as HTMLCanvasElement,
+            resizedDetections
+          );
+          const minProbability = 0.05;
+          faceapi.draw.drawFaceExpressions(
+            outCanvasElement as HTMLCanvasElement,
+            resizedDetections,
+            minProbability
+          );
+          // const descriptor = singleResult[0];
+          const faceMatcher = new faceapi.FaceMatcher(detectionResult);
+          // const bestMatch = faceMatcher.findBestMatch(singleResult.descriptor);
+          console.log(faceMatcher); //output
+          this.reQueSession();
+        } else {
+          this.reQueSession();
+        }
+      };
+      if (!this.didMount) return;
+      this.setState({ input_image: image_url });
+    }
+  }
+
+  private reQueSession() {
+    setTimeout(() => {
+      if (!this.didMount) return;
+      if (this.state.is_running_process) {
+        this.queSession(this.state.session_id);
+      }
+    }, 100);
   }
 
   private onStopSessionBtnClick() {
@@ -236,6 +210,12 @@ export default class Main extends React.Component<
           <div className="admin-lay mdm-shadow">
             <div className="title-lay">Session</div>
             {/* <div className="session-id-lay"> */}
+            <i
+              className="las la-spinner la-spin progress-icon"
+              style={{
+                display: this.state.is_initializing ? "flex" : "none",
+              }}
+            ></i>
             <span className="session-id-title">Session Id</span>
             <input
               className="session-id-input input"
